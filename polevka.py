41874d459cc5120cd1e84a1743c691cca26f60d0
Magdalena Berková <172276901+Majdik21@users.noreply.github.com>
2025-12-10 09:06:23 +0100

    Add files via upload

--- polevka.py
+++ polevka.py
@@ -1,14 +1,32 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+import json
 import requests
 
+file= 'recept.json'
+url = 'https://souhrada.github.io/bsoup-exam/'
 
 def main():
 
+    bs(response.content, "html.parser") #<--- Úkol: popiš krátce, co tohle dělá = nacte content na strance
+    
+    response = requests.get(url)
 
-    # response = requests.get(url)
+    list = bs.find_all('h2')
 
-    # BeautifulSoup(response.content, "html.parser") <--- Úkol: popiš krátce, co tohle dělá
+    ingredients = list[0,1,2,3]
+
+    for i in ingredients:
+        print(i)
+
+    json.dump(ingredients, file, indent =4 )
+
+    
+    
+    
+
 
 
 if __name__ == "__main__":
     main()
+
+
--- secret.py
+++ secret.py
@@ -6,9 +6,21 @@ import os
 
 def main():
 
-    # with sync_playwright() as p:
+    with sync_playwright() as p:
 
-        # browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page
+
+        page.goto('https://souhrada.github.io/playwright-exam/')
+        
+        page.fill('input[id="login"]', 'Jarmil')
+        page.fill('input[id="pass"]', 'Admin123')
+
+        page.click('button[class="login-btn"]')
+
+
+        text = page.locator('p[class="super-secret-text"]').text_content()
+        print(text)
 
         # !!!
         # na page.locator(selector) se dá použít funkce .text_content(), která vypíše text daného prvku
