097fed7d5f37d19d66abe65d22710c9cebedb218
JS <souhrada@trebesin.cz>
2026-01-04 19:59:39 +0100

    oprava

--- polevka.py
+++ polevka.py
@@ -2,31 +2,33 @@ from bs4 import BeautifulSoup as bs
 import json
 import requests
 
-file= 'recept.json'
-url = 'https://souhrada.github.io/bsoup-exam/'
+file = "recept.json"
+url = "https://souhrada.github.io/bsoup-exam/"
+
 
 def main():
 
-    bs(response.content, "html.parser") #<--- Úkol: popiš krátce, co tohle dělá = nacte content na strance
-    
-    response = requests.get(url)
+    # chybí vytvoření proměnné, např. soup pro uložení kódu níže
+    bs(
+        response.content, "html.parser"
+    )  # <--- Úkol: popiš krátce, co tohle dělá = nacte content na strance
 
-    list = bs.find_all('h2')
+    response = requests.get(
+        url
+    )  # vytvoření response musí být před tím, než jej použijete
 
-    ingredients = list[0,1,2,3]
+    list = bs.find_all("h2")
+
+    ingredients = list[
+        0, 1, 2, 3
+    ]  # takto nelze pracovat s listem, proč raději nepoužijete ve for loopu range(4)?
 
     for i in ingredients:
         print(i)
 
-    json.dump(ingredients, file, indent =4 )
-
-    
-    
-    
-
+    # chybí witf open a syntax jsonu
+    json.dump(ingredients, file, indent=4)
 
 
 if __name__ == "__main__":
     main()
-
-
--- secret.py
+++ secret.py
@@ -2,22 +2,19 @@ from playwright.sync_api import sync_playwright
 import os
 
 
-
-
 def main():
 
     with sync_playwright() as p:
 
         browser = p.chromium.launch(headless=False)
-        page = browser.new_page
+        page = browser.new_page  # chybí závorky ()
 
-        page.goto('https://souhrada.github.io/playwright-exam/')
-        
-        page.fill('input[id="login"]', 'Jarmil')
-        page.fill('input[id="pass"]', 'Admin123')
+        page.goto("https://souhrada.github.io/playwright-exam/")
+
+        page.fill('input[id="login"]', "Jarmil")
+        page.fill('input[id="pass"]', "Admin123")
 
         page.click('button[class="login-btn"]')
-
 
         text = page.locator('p[class="super-secret-text"]').text_content()
         print(text)
@@ -26,9 +23,8 @@ def main():
         # na page.locator(selector) se dá použít funkce .text_content(), která vypíše text daného prvku
         # !!!
 
-
         # browser.close()
-    
+
 
 if __name__ == "__main__":
     main()
